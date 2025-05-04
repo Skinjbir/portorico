a5a801c2232e947de0c60db3fd69a898961c793f
Skinjbir <mohammedredatarmidi@gmail.com>
2025-05-04 15:50:03 +0100

    add testi some lines

--- src/Components/data/testemonials.js
+++ src/Components/data/testemonials.js
@@ -1,7 +1,11 @@
 // src/data/testimonials.js
 
 const testimonials = [
-
+    {
+      name: "Haitam Zouhair",
+      title: "Software Engineer at HPS",
+      feedback: "Mohammed-Reda was a pleasure to work with. He consistently showed initiative and delivered strong results. His drive, reliability, and collaborative spirit made a real difference",
+    },
     {
       name: "Fortune Vierra",
       title: "CEO & Founder of Beladed Inc.",
